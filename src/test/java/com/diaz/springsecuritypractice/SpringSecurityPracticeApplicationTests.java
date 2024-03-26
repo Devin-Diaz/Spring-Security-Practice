08778a17fd21f6b503e6e86159e4aaf66d9ebc79
Devin Diaz <diazdevin7@outlook.com>
2024-03-25 22:03:14 -0400

    adding project to github

---
+++ src/test/java/com/diaz/springsecuritypractice/SpringSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diaz.springsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
