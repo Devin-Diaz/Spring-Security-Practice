5f60d4168d3605c18694eb024b26d0865dcd210c
Devin Diaz <diazdevin7@outlook.com>
2024-03-26 22:15:42 -0400

    added more documentation about @Builder and @Data annotations provided by lombok

--- src/main/java/com/diaz/springsecuritypractice/dto/Product.java
+++ src/main/java/com/diaz/springsecuritypractice/dto/Product.java
@@ -8,7 +8,16 @@ and hashcode() methods.
 equals() and hashcode() - equals() is a boolean method that returns whether and object is equal to another. hashcode()
 returns an integer (hash) that helps us find an object faster. hashing is utilized by HashMaps thus the O(1) TC.
 
+@Builder allows you to make instances of a class within the class. An example would be the following:
 
+Person person = Person.builder()
+                      .name("John Doe")
+                      .age(30)
+                      .build()
+
+Additionally comes with setter methods as well. @Builder simplifies the readability of method chaining, in this
+case with setter methods. We set the values of Person w/ name: John Doe and age: 30. Usually another example of
+method chaining is on our Spring Security Config class for future reference.
 */
 
 @Data
@@ -21,6 +30,5 @@ public class Product {
     private String productName;
     private int quantity;
     private double price;
-
 
 }
