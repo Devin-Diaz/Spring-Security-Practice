d345791c0f28ee73763facba03fce772d57d58e1
Devin Diaz <diazdevin7@outlook.com>
2024-03-26 22:26:55 -0400

    will begin config class tmr

---
+++ src/main/java/com/diaz/springsecuritypractice/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.diaz.springsecuritypractice.security;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // 13:01
+}
